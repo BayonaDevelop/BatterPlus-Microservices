33c1a5d81100058947855eb3ba86193853153c3c
Fernando Bayona <bayona.develop@gmail.com>
2022-07-09 16:28:57 -0500

    Server Gateway

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "API"
-include("Infrastructure", "Infrastructure:Server-Config", "Infrastructure:Server-Eureka")
+include("Infrastructure", "Infrastructure:Server-Config", "Infrastructure:Server-Eureka", "Infrastructure:Server-Gateway")
 include("Microservices", "Microservices:Utils", "Microservices:Services")
