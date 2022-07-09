0ab4b01029960ee073ee59ea881e233c2fd4dc68
Fernando Bayona <bayona.develop@gmail.com>
2022-07-09 15:39:42 -0500

    Eureka Server

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,3 @@
 rootProject.name = "API"
-include("Infrastructure", "Infrastructure:Server-Config")
+include("Infrastructure", "Infrastructure:Server-Config", "Infrastructure:Server-Eureka")
 include("Microservices", "Microservices:Utils", "Microservices:Services")
